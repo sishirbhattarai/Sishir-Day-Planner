5f0518f3dc2b45b67bc3e68f0539abc65febead4
Sishir <sishir.bhattarai@outlook.com>
2020-10-17 08:57:07 -0600

    added rows and containers

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+//Use of `moment()` to display the day of the week at the very top of the page
 
-function day(){
-    var today = moment().format('dddd MMMM Do YYYY');
-    $("#currentDay").text(today);
-    console.log(today)
-    
+function day() {
+  var today = moment().format("dddd MMMM Do YYYY");
+  $("#currentDay").text(today);
+  console.log(today);
 }
-
 day();
+
