70d7d7843f8317707722d7f1f78422affd5b12af
Sishir <sishir.bhattarai@outlook.com>
2020-10-17 17:52:43 -0600

    added code

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //Use of `moment()` to display the day of the week at the very top of the page
 
 function day() {
-  var today = moment().format("dddd MMMM Do YYYY");
+  var today = moment().format('dddd MMMM Do YYYY h:mm:ss a');
   $("#currentDay").text(today);
   console.log(today);
 }
 day();
+
+var currentTime = moment().calendar(); 
+console.log(currentTime);
+
+// var startTime = 9;
+// var endTime = 5;
+
+// var time = 
 
